e9124764ae23ade2cd9b8fbe60948b22d9b6c1d0
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2021-07-17 12:22:53 -0400

    feat(404): updated 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,37 @@
 import React from "react";
+import {
+  PageContent,
+  Heading,
+  Paragraph,
+  Stack,
+  Link as BumbagLink,
+  Set,
+} from "bumbag";
+import { Link } from "react-router-dom";
 
 const NoMatchPage: React.FC = () => {
+  const linkProps = BumbagLink.useProps();
+
   return (
-    <div>
-      <h1>404: page not found.</h1>
-    </div>
+    <PageContent>
+      <Stack>
+        <Heading>Oh No!</Heading>
+        <Paragraph>
+          Looks like the page you were looking for doesn't exist.
+        </Paragraph>
+        <Set>
+          <Link {...linkProps} to="/">
+            Home
+          </Link>
+          <Link {...linkProps} to="/login">
+            Login
+          </Link>
+          <Link {...linkProps} to="/signup">
+            Sign Up
+          </Link>
+        </Set>
+      </Stack>
+    </PageContent>
   );
 };
 
