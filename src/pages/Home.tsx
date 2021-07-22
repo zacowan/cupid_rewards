0d831483fa393d6b0299b163b84a910bad848e70
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2021-07-22 09:39:16 -0400

    feat(home): added delete confirmation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
 import React, { useState, useEffect, useContext } from "react";
-import { PageContent, Heading, Stack, Tabs, Card, Set, Button } from "bumbag";
+import {
+  PageContent,
+  Heading,
+  Stack,
+  Tabs,
+  Card,
+  Set,
+  Button,
+  Dialog,
+  Modal,
+} from "bumbag";
 import { Link } from "react-router-dom";
 
 import { AuthContext } from "../components/AuthContext";
@@ -8,9 +18,14 @@ import DeleteRewardsPage from "../api/DeleteRewardsPage";
 import { RewardsPageWithID } from "../api/utils";
 
 const HomePage: React.FC = () => {
+  const [rewardToDelete, setRewardToDelete] = useState<string>();
   const { user } = useContext(AuthContext);
   const [myPages, setMyPages] = useState<Array<RewardsPageWithID>>([]);
   const [myRewards, setMyRewards] = useState<Array<RewardsPageWithID>>([]);
+  const deleteButtonProps = Button.useProps({
+    variant: "ghost",
+    palette: "danger",
+  });
   const createButtonProps = Button.useProps({
     width: "100%",
     palette: "primary",
@@ -28,16 +43,16 @@ const HomePage: React.FC = () => {
     loadData();
   }, [user]);
 
-  const handleDeletePage = async (id: string) => {
-    try {
-      await DeleteRewardsPage(id);
-      let newPages: Array<RewardsPageWithID> = [];
-      myPages.forEach((p) => {
-        if (p.id !== id) newPages.push(p);
-      });
-      setMyPages(newPages);
-    } catch (error) {
-      console.log(error);
+  const handleDeletePage = async () => {
+    const id = rewardToDelete;
+    if (id) {
+      try {
+        await DeleteRewardsPage(id);
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
@@ -55,28 +70,41 @@ const HomePage: React.FC = () => {
               <Link {...createButtonProps} to="/create-rewards-page">
                 Create
               </Link>
-              {myPages.map((p) => (
-                <Card
-                  key={p.id}
-                  title={p.title}
-                  footer={
-                    <Set>
-                      <Link {...manageButtonProps} to="/manage">
-                        Manage
-                      </Link>
-                      <Button
-                        onClick={() => handleDeletePage(p.id)}
-                        variant="ghost"
-                        palette="danger"
-                      >
-                        Delete
-                      </Button>
-                    </Set>
-                  }
+              <Modal.State>
+                <Dialog.Modal
+                  showActionButtons
+                  baseId="confirmDelete"
+                  title="Confirm Delete"
+                  actionButtonsProps={{
+                    submitText: "Delete",
+                    palette: "danger",
+                    onClickSubmit: handleDeletePage,
+                  }}
                 >
-                  {p.description}
-                </Card>
-              ))}
+                  Are you sure you want to delete this reward's page?
+                </Dialog.Modal>
+                {myPages.map((p) => (
+                  <Card
+                    key={p.id}
+                    title={p.title}
+                    footer={
+                      <Set>
+                        <Link {...manageButtonProps} to="/manage">
+                          Manage
+                        </Link>
+                        <Modal.Disclosure
+                          onClick={() => setRewardToDelete(p.id)}
+                          {...deleteButtonProps}
+                        >
+                          Delete
+                        </Modal.Disclosure>
+                      </Set>
+                    }
+                  >
+                    {p.description}
+                  </Card>
+                ))}
+              </Modal.State>
             </Stack>
           </Tabs.Panel>
           <Tabs.Panel tabId="my-rewards" marginTop="major-2">
